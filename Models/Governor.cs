de2794d239eb68ca7a0da75d01d163103be76cf5
Jaiden Rothrock <jaiden_rothrock@hotmail.com>
2026-09-14 19:35:43 -0500

    started Data Structure

---
+++ Models/Governor.cs
@@ -0,0 +1,9 @@
+public class Governor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ColonyId { get; set; }
+    public bool Acitve { get; set; }
+
+    public List<> GovernorHistory { get; set; }
+}
