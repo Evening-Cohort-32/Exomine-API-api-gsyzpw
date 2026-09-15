5e725bc69156e53df4524fe3c58dd96844307fb0
Jaiden Rothrock <jaiden_rothrock@hotmail.com>
2026-09-14 21:16:07 -0500

    further defined FacilityInventory and Transaction Class Models

--- Models/FacilityInventory.cs
+++ Models/FacilityInventory.cs
@@ -7,4 +7,6 @@ public class FacilityInventory
     public int MineralId { get; set; }
     public int SaleQuantity { get; set; }
 
+    public MiningFacility MiningFacility { get; set; }
+    public Mineral Mineral { get; set; }
 }
